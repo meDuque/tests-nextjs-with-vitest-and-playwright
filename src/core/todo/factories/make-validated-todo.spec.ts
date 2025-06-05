0906356f979db177e2eb2f12f26da5cb57b13050
¡Duque <atanaelleonardo@gmail.com>
2025-06-05 18:37:08 -0300

    refactor makeValidatedTodo spec to use makeMocks function

--- src/core/todo/factories/make-validated-todo.spec.ts
+++ src/core/todo/factories/make-validated-todo.spec.ts
@@ -3,22 +3,26 @@ import { makeValidatedTodo } from './make-validated-todo'
 
 describe('makeValidatedTodo (unit)', () => {
   it('should calls sanitizeStr function with correct value', () => {
-    // Arrange
-    const description = 'lorem ipsum'
-    const sanitizeStrSpy = vi.spyOn(sanitizeStrMod, 'sanitizeStr').mockReturnValue(description)
+    const { description, sanitizeStrSpy } = makeMocks()
 
-    // Act
     makeValidatedTodo(description)
-
-    // Assert
     expect(sanitizeStrSpy).toHaveBeenCalledExactlyOnceWith(description)
-    expect(sanitizeStrSpy).toHaveBeenCalledTimes(1)
-    expect(sanitizeStrSpy).toHaveBeenCalledWith(description)
   })
 
-  // it('should calls validatedTodoDescription with sanitizeStr return', () => {})
+  it('should calls validatedTodoDescription with sanitizeStr return', () => {
+    // Arrange
+  })
 
   // it('should calls makeNewTodo if validatedTodoDescription return success', () => {})
 
   // it('should return validatedTodoDescription.errors if validation fails', () => {})
 })
+
+const makeMocks = (description = 'lorem ipsum') => {
+  const sanitizeStrSpy = vi.spyOn(sanitizeStrMod, 'sanitizeStr').mockReturnValue(description)
+
+  return {
+    description,
+    sanitizeStrSpy,
+  }
+}
