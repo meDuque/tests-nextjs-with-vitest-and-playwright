01d7caf00d367b14931e55e16afee71fc8c12671
¡Duque <atanaelleonardo@gmail.com>
2025-06-05 19:59:17 -0300

    test: add test case for validation failure in makeValidatedTodo

--- src/core/todo/factories/make-validated-todo.spec.ts
+++ src/core/todo/factories/make-validated-todo.spec.ts
@@ -1,7 +1,7 @@
 import * as sanitizeStrMod from '@/utils/sanitize-str'
 import * as validateTodoDescriptionMod from '../schemas/validate-todo-description'
 import * as makeNewTodoMod from './make-new-todo'
-import { makeValidatedTodo, type ValidTodo } from './make-validated-todo'
+import { makeValidatedTodo, type InvalidTodo, type ValidTodo } from './make-validated-todo'
 import type { Todo } from '../schemas/todo.contract'
 
 describe('makeValidatedTodo (unit)', () => {
@@ -32,7 +32,14 @@ describe('makeValidatedTodo (unit)', () => {
     expect(result.data.createdAt).toMatch(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/)
   })
 
-  // it('should return validatedTodoDescription.errors if validation fails', () => {})
+  it('should return validatedTodoDescription.errors if validation fails', () => {
+    const { errors, description, validateTodoDescriptionSpy } = makeMocks()
+    validateTodoDescriptionSpy.mockReturnValue({ errors, success: false })
+
+    const result = makeValidatedTodo(description) as InvalidTodo
+
+    expect(result).toStrictEqual({ errors, success: false })
+  })
 })
 
 const makeMocks = (description = 'lorem ipsum') => {
@@ -41,17 +48,23 @@ const makeMocks = (description = 'lorem ipsum') => {
     description: description,
     createdAt: new Date().toISOString(),
   }
+
+  const errors = ['any', 'error']
+
   const sanitizeStrSpy = vi.spyOn(sanitizeStrMod, 'sanitizeStr').mockReturnValue(description)
+
   const validateTodoDescriptionSpy = vi
     .spyOn(validateTodoDescriptionMod, 'validateTodoDescription')
     .mockReturnValue({
       errors: [],
       success: true,
     })
+
   const makeNewTodoSpy = vi.spyOn(makeNewTodoMod, 'makeNewTodo').mockReturnValue(todo)
 
   return {
     todo,
+    errors,
     description,
     sanitizeStrSpy,
     validateTodoDescriptionSpy,
