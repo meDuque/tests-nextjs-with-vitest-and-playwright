16848f90a2540709b067ef86aedeb00713f78f49
¡Duque <atanaelleonardo@gmail.com>
2025-06-05 18:30:58 -0300

    test: add import and test assertions for sanitizeStr function in makeValidatedTodo spec

--- src/core/todo/factories/make-validated-todo.spec.ts
+++ src/core/todo/factories/make-validated-todo.spec.ts
@@ -1,20 +1,24 @@
+import * as sanitizeStrMod from '@/utils/sanitize-str'
 import { makeValidatedTodo } from './make-validated-todo'
 
 describe('makeValidatedTodo (unit)', () => {
   it('should calls sanitizeStr function with correct value', () => {
     // Arrange
     const description = 'lorem ipsum'
+    const sanitizeStrSpy = vi.spyOn(sanitizeStrMod, 'sanitizeStr')
 
     // Act
     makeValidatedTodo(description)
 
     // Assert
-    expect(description).toBe('lorem ipsum')
+    expect(sanitizeStrSpy).toHaveBeenCalledExactlyOnceWith(description)
+    expect(sanitizeStrSpy).toHaveBeenCalledTimes(1)
+    expect(sanitizeStrSpy).toHaveBeenCalledWith(description)
   })
 
-  it('should calls validatedTodoDescription with sanitizeStr return', () => {})
+  // it('should calls validatedTodoDescription with sanitizeStr return', () => {})
 
-  it('should calls makeNewTodo if validatedTodoDescription return success', () => {})
+  // it('should calls makeNewTodo if validatedTodoDescription return success', () => {})
 
-  it('should return validatedTodoDescription.errors if validation fails', () => {})
+  // it('should return validatedTodoDescription.errors if validation fails', () => {})
 })
