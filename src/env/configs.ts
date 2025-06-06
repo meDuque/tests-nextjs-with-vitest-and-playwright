c61b6e09ac1b8105fb7b50144f1c7819d723032d
¡Duque <atanaelleonardo@gmail.com>
2025-06-06 13:20:30 -0300

    feat: add dotenv executor for TypeScript and environment configuration functions

--- src/env/configs.ts
+++ src/env/configs.ts
@@ -16,3 +16,36 @@ const envConfigs = {
     currentEnv: 'e2e',
   },
 } as const
+
+type ConfigsByEnv = {
+  readonly databaseFile: string
+  readonly currentEnv: keyof EnvConfigs
+}
+
+type EnvConfigs = typeof envConfigs
+type AllowedEnvKeys = keyof EnvConfigs
+
+function isValidEnv(env: string): env is AllowedEnvKeys {
+  return Object.keys(envConfigs).includes(env)
+}
+
+export function checkEnv(): AllowedEnvKeys {
+  const currentEnv = process.env.CURRENT_ENV
+
+  if (!currentEnv || !isValidEnv(currentEnv)) {
+    throw new Error('Verifique os .env* e os valores em src/env/configs.ts')
+  }
+
+  return currentEnv
+}
+
+export function getFullEnv() {
+  const currentEnv = checkEnv()
+  return envConfigs[currentEnv]
+}
+
+export function getEnv<C extends keyof ConfigsByEnv>(key: C) {
+  const currentEnv = checkEnv()
+  const value = envConfigs[currentEnv][key]
+  return value
+}
